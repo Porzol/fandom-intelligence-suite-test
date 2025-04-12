9ecb370a51212995e8cc019cbd49c24c08a40134
David Matthew Antonio <i_davidmatthew.antonio@stratpoint.com>
2025-04-12 15:55:13 +0800

    Changed the run command

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
-from app.core.config import settings
+import app.core.config as  config
 from app.db.base import get_db
 from app.models.user import User, UserRole
 from app.schemas.user import TokenData
@@ -18,7 +18,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
